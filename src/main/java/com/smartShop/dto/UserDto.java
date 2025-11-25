188ae84a6f3ddc42948c9dbef56e8dc429d6153d
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-25 14:12:19 +0100

    l'implémentation du dto de l'entité User

--- src/main/java/com/smartShop/dto/UserDto.java
+++ src/main/java/com/smartShop/dto/UserDto.java
@@ -1,4 +1,17 @@
 package com.smartShop.dto;
 
-public enum UserDto {
+
+import com.smartShop.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Integer id;
+    private String username;
+    private Role role;
 }
+
