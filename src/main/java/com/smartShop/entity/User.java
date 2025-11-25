fd68f5716b9ab2b419da4f64f08f938cd43ac4ed
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-25 14:09:26 +0100

    la'impÃlémentation de la classe User

--- src/main/java/com/smartShop/entity/User.java
+++ src/main/java/com/smartShop/entity/User.java
@@ -1,4 +1,31 @@
 package com.smartShop.entity;
 
+
+import com.smartShop.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role; // Role peut être ADMIN ou CLIENT
 }
+
