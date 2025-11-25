70e45fb5abf50a7cbcb408d7168e7fe742dca34e
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-25 16:06:53 +0100

    aajouter default method au clientMapper pour mapper les userid vers user

--- src/main/java/com/smartShop/mapper/ClientMapper.java
+++ src/main/java/com/smartShop/mapper/ClientMapper.java
@@ -2,11 +2,28 @@ package com.smartShop.mapper;
 
 import com.smartShop.dto.ClientDto;
 import com.smartShop.entity.Client;
+import com.smartShop.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface ClientMapper {
 
+    @Mapping(source = "user.id", target = "userId")
     ClientDto toDTO(Client client);
+
+    @Mapping(source = "userId", target = "user", qualifiedByName = "mapUserIdToUser")
     Client toEntity(ClientDto dto);
+
+
+    //default methode pour mapper UserId to User
+    @Named("mapUserIdToUser")
+    default User mapUserIdToUser(Integer userId) {
+        if (userId == null) return null;
+        User user = new User();
+        user.setId(userId);
+        return user;
+    }
+
 }
