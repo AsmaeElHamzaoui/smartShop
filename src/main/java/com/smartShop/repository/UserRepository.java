2431b3ea3da4e8cc44098924dce7c5b18fb5154b
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-25 14:15:20 +0100

    la'impÃlémentation de l'interface UserRepository

--- src/main/java/com/smartShop/repository/UserRepository.java
+++ src/main/java/com/smartShop/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.smartShop.repository;
 
-public interface UserRepository {
+
+import com.smartShop.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
