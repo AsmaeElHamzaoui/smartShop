2b43608a4f197bee1170bdb74b4cac2c86ced7a0
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-27 00:13:37 +0100

    Rendre la classe UnauthorizedActionException hérite de la classe RuntimeException ainsi la création de la méthode UnauthorizedActionException

--- src/main/java/com/smartShop/exception/UnauthorizedActionException.java
+++ src/main/java/com/smartShop/exception/UnauthorizedActionException.java
@@ -1,4 +1,9 @@
 package com.smartShop.exception;
 
-public class UnauthorizedActionException {
+
+// 401 - L'utilisateur tente d'accéder à une ressource sans être authentifié
+public class UnauthorizedActionException extends RuntimeException {
+    public UnauthorizedActionException(String message) {
+        super(message);
+    }
 }
