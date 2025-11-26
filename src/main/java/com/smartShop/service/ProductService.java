09c1ca9877f2182d3205b415e3bc9151023e6eb5
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-26 09:35:51 +0100

    l'implémentation de la méthode create pour la création d'un produit

--- src/main/java/com/smartShop/service/ProductService.java
+++ src/main/java/com/smartShop/service/ProductService.java
@@ -1,4 +1,27 @@
 package com.smartShop.service;
 
+import com.smartShop.dto.ProductDto;
+import com.smartShop.entity.Product;
+import com.smartShop.mapper.ProductMapper;
+import com.smartShop.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+
+    // CREATE
+    public ProductDto create(ProductDto dto) {
+        Product product = productMapper.toEntity(dto);
+        Product saved = productRepository.save(product);
+        return productMapper.toDTO(saved);
+    }
 }
