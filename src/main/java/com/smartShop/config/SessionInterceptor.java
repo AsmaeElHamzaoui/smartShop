9ad27ae7a3208d71dcc693fb2327cfaf4ffa0532
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-27 00:34:56 +0100

    Rendre la classe SessionInterceptor implémente l'interface HandlerInterceptor

--- src/main/java/com/smartShop/config/SessionInterceptor.java
+++ src/main/java/com/smartShop/config/SessionInterceptor.java
@@ -1,4 +1,15 @@
 package com.smartShop.config;
 
-public class SessionInterceptor {
+import com.smartShop.dto.UserDto;
+import com.smartShop.enums.Role;
+import com.smartShop.exception.ForbiddenException;
+import com.smartShop.exception.UnauthorizedActionException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class SessionInterceptor implements HandlerInterceptor {
 }
