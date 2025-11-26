1568cbb6ba108a3646a9d2dcc3f76501dd490724
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-26 15:46:05 +0100

    l'ajout des annotations ainsi que les importations pour faire fonctionner le controleur d'authentification

--- src/main/java/com/smartShop/controller/authController.java
+++ src/main/java/com/smartShop/controller/authController.java
@@ -1,4 +1,14 @@
 package com.smartShop.controller;
 
+
+import com.smartShop.dto.UserDto;
+import com.smartShop.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class authController {
 }
