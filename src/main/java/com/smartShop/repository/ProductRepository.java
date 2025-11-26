a4f23df9be65de39510d2de94c18d14bad44f83d
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-26 09:31:08 +0100

    l'ajout de l'annotation @Repository dans le repo ProductRepository

--- src/main/java/com/smartShop/repository/ProductRepository.java
+++ src/main/java/com/smartShop/repository/ProductRepository.java
@@ -4,5 +4,6 @@ import com.smartShop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
