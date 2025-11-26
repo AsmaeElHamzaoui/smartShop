3812d598ae64a83510045613c414c4c045db21ab
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-27 00:14:41 +0100

    l'implémentation de la classe ForbiddenException pour gérer les exceptions 403

---
+++ src/main/java/com/smartShop/exception/ForbiddenException.java
@@ -0,0 +1,8 @@
+package com.smartShop.exception;
+
+// 403 - L'utilisateur est authentifié mais n’a pas les droits pour accéder à cette ressource
+public class ForbiddenException extends RuntimeException {
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
