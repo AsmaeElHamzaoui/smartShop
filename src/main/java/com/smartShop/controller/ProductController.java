f8b03892994c61bf4b3c6a760061493e5cbb9e3c
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-26 09:47:48 +0100

    l'ajout des annotations necessaires ainsi que l'importation des classes à utiliser dans le controleur des produits

--- src/main/java/com/smartShop/controller/ProductController.java
+++ src/main/java/com/smartShop/controller/ProductController.java
@@ -1,4 +1,18 @@
 package com.smartShop.controller;
 
+import com.smartShop.dto.ProductDto;
+import com.smartShop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
 }
