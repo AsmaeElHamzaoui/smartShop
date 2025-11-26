4ac924368caf57cbea66d3ab7ef573875808e03f
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-26 11:17:53 +0100

    la'ajout des annotations necessaires ainsi que l'importation des classes pour faire fonctionner le controleur de user

--- src/main/java/com/smartShop/controller/UserController.java
+++ src/main/java/com/smartShop/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.smartShop.controller;
 
+import com.smartShop.dto.RegisterRequest;
+import com.smartShop.dto.UserDto;
+import com.smartShop.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
 }
