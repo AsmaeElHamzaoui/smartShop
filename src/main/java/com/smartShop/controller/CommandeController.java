4c8e60df0027b64cf71c99de8bf9b041acb1300d
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-28 00:12:20 +0100

    la gestion de  l'endpoint pour gérer la création d'une commande

--- src/main/java/com/smartShop/controller/CommandeController.java
+++ src/main/java/com/smartShop/controller/CommandeController.java
@@ -1,50 +1,27 @@
 package com.smartShop.controller;
+
 import com.smartShop.dto.CommandeDto;
 import com.smartShop.service.CommandeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/commandes")
+@RequestMapping("/commandes")
 @RequiredArgsConstructor
 public class CommandeController {
 
-    private final CommandeService commandeService;
+    private final CommandeService service;
 
     // CREATE
     @PostMapping
     public ResponseEntity<CommandeDto> create(@RequestBody CommandeDto dto) {
-        return ResponseEntity.ok(commandeService.create(dto));
+        CommandeDto created = service.create(dto);
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
 
-    // READ ALL
-    @GetMapping
-    public ResponseEntity<List<CommandeDto>> getAll() {
-        return ResponseEntity.ok(commandeService.getAll());
-    }
 
-    // READ ONE
-    @GetMapping("/{id}")
-    public ResponseEntity<CommandeDto> getById(@PathVariable Integer id) {
-        return ResponseEntity.ok(commandeService.getById(id));
-    }
-
-    // UPDATE
-    @PutMapping("/{id}")
-    public ResponseEntity<CommandeDto> update(
-            @PathVariable Integer id,
-            @RequestBody CommandeDto dto
-    ) {
-        return ResponseEntity.ok(commandeService.update(id, dto));
-    }
-
-    // DELETE
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Integer id) {
-        commandeService.delete(id);
-        return ResponseEntity.noContent().build();
-    }
 }
