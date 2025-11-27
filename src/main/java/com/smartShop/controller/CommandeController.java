455e2056c73cae0b342c55bcb118a115b6bfebd5
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-27 10:23:00 +0100

    l'importation des classes necessaires pour le fonctionnement de service commande

--- src/main/java/com/smartShop/controller/CommandeController.java
+++ src/main/java/com/smartShop/controller/CommandeController.java
@@ -1,4 +1,15 @@
 package com.smartShop.controller;
+import com.smartShop.dto.CommandeDto;
+import com.smartShop.service.CommandeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/commandes")
+@RequiredArgsConstructor
 public class CommandeController {
+
 }
