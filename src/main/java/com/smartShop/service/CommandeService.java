e385d58fe239c89e2813deb8bf0af83504b0d349
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-27 10:13:15 +0100

    la création de la classe CommandeService en important les classes necessaire pour le fonctionnement de notre sevice

---
+++ src/main/java/com/smartShop/service/CommandeService.java
@@ -0,0 +1,21 @@
+package com.smartShop.service;
+
+
+import com.smartShop.dto.CommandeDto;
+import com.smartShop.entity.Client;
+import com.smartShop.entity.Commande;
+import com.smartShop.mapper.CommandeMapper;
+import com.smartShop.repository.ClientRepository;
+import com.smartShop.repository.CommandeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CommandeService {
+
+
+}
