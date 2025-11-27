024d00fe4acc84fa1046f02d9ed00b8da5902364
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-28 00:38:23 +0100

    la'implémentation de la classe Paiement

--- src/main/java/com/smartShop/entity/Paiement.java
+++ src/main/java/com/smartShop/entity/Paiement.java
@@ -1,4 +1,45 @@
 package com.smartShop.entity;
 
+import com.smartShop.enums.PaymentStatus;
+import com.smartShop.enums.TypePaiement;
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "paiements")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Paiement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    // Relation ManyToOne vers Commande
+    @ManyToOne
+    @JoinColumn(name = "commande_id", nullable = false)
+    private Commande commande;
+
+    @Column(name = "numero_paiement", nullable = false)
+    private Integer numeroPaiement;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal montant;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_paiement", nullable = false)
+    private PaymentStatus statusPaiement;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type_paiement", nullable = false)
+    private TypePaiement typePaiement;
+
+    @Column(name = "date_paiement", nullable = false)
+    private LocalDateTime datePaiement;
+
+    @Column(name = "date_encaissement")
+    private LocalDateTime dateEncaissement;
 }
