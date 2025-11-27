0a094569f6c2f9cd4106cb9ebf255f22c0b3e70d
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-27 09:53:47 +0100

    Rendre le repo du commande hérite du JpaRepository en ajoutant aussi l'annotation @Repository

--- src/main/java/com/smartShop/repository/CommandeRepository.java
+++ src/main/java/com/smartShop/repository/CommandeRepository.java
@@ -1,4 +1,10 @@
 package com.smartShop.repository;
 
-public interface CommandeRepository {
+import com.smartShop.entity.Commande;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommandeRepository extends JpaRepository<Commande, Integer> {
 }
