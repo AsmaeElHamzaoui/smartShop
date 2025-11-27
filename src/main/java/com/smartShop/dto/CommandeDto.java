5894b26453e5cf5d19422c04b5ac2938e5caa7e5
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-27 15:19:46 +0100

    la création de l'entité de OrderItem

--- src/main/java/com/smartShop/dto/CommandeDto.java
+++ src/main/java/com/smartShop/dto/CommandeDto.java
@@ -1,5 +1,6 @@
 package com.smartShop.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.smartShop.enums.OrderStatus;
 import lombok.Data;
 import java.math.BigDecimal;
@@ -10,11 +11,21 @@ public class CommandeDto {
 
     private Integer id;
     private Integer clientId;
+
     private LocalDate date;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private BigDecimal sousTotal;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Integer remise;
+
+    //Tva : au départ 20 mais on peut la configurer
     private BigDecimal tva;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private BigDecimal total;
+
     private String codePromo;
     private OrderStatus statut;
     private BigDecimal montantRestant;
---
+++ src/main/java/com/smartShop/entity/OrderItem.java
@@ -0,0 +1,39 @@
+package com.smartShop.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    // Relation ManyToOne vers Produit
+    @ManyToOne
+    @JoinColumn(name = "produit_id", nullable = false)
+    private Product product;
+
+    // Relation ManyToOne vers Commande
+    @ManyToOne
+    @JoinColumn(name = "commande_id", nullable = false)
+    private Commande commande;
+
+    @Column(nullable = false)
+    private Integer quantite;
+
+    @Column(nullable = false)
+    private BigDecimal prixUnitaire;
+
+    @Column(nullable = false)
+    private BigDecimal totalLigne;
+}
