200c74892532ebf6c2506750168a37506e3baf85
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-11-28 15:24:11 +0100

    la création du paiement controleur ainsi que la création de lÃa méthode créerPaiement pour gérer l'endpoint de création d'un paiement

--- src/main/java/com/smartShop/controller/PaiementController.java
+++ src/main/java/com/smartShop/controller/PaiementController.java
@@ -1,4 +1,27 @@
 package com.smartShop.controller;
 
+import com.smartShop.dto.PaiementDto;
+import com.smartShop.enums.PaymentStatus;
+import com.smartShop.service.PaiementService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/paiements")
+@RequiredArgsConstructor
 public class PaiementController {
+
+    private final PaiementService service;
+
+    @PostMapping
+    public ResponseEntity<PaiementDto> creerPaiement(@RequestBody PaiementDto dto) {
+        return ResponseEntity.ok(service.creerPaiement(dto));
+    }
+
+
 }
